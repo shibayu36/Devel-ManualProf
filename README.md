2d189c3dbd857b61bc3d83297edb8a0198d95fd7
shibayu36 <shibayu36@gmail.com>
2018-09-20 19:26:49 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Devel::ManualProf - It's new $module
+
+# SYNOPSIS
+
+    use Devel::ManualProf;
+
+# DESCRIPTION
+
+Devel::ManualProf is ...
+
+# LICENSE
+
+Copyright (C) shibayu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+shibayu36 <shibayu36@gmail.com>
---
+++ lib/Devel/ManualProf.pm
@@ -0,0 +1,39 @@
+package Devel::ManualProf;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Devel::ManualProf - It's new $module
+
+=head1 SYNOPSIS
+
+    use Devel::ManualProf;
+
+=head1 DESCRIPTION
+
+Devel::ManualProf is ...
+
+=head1 LICENSE
+
+Copyright (C) shibayu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shibayu36 E<lt>shibayu36@gmail.comE<gt>
+
+=cut
+
