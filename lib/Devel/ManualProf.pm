e0302da22fc08c7229923d0ac3044988d2e750e5
shibayu36 <shibayu36@gmail.com>
2018-09-20 19:35:12 +0900

    implements

--- lib/Devel/ManualProf.pm
+++ lib/Devel/ManualProf.pm
@@ -1,11 +1,41 @@
 package Devel::ManualProf;
-use 5.008001;
 use strict;
 use warnings;
+use utf8;
 
 our $VERSION = "0.01";
 
+use feature qw(say);
 
+use Time::Moment;
+
+sub new {
+    my ($class, $log_filename) = @_;
+    open(my $log_fh, ">>", $log_filename) or die "Can't open > $log_filename: $!";
+    bless {
+        log_fh => $log_fh,
+        timers => {},
+    }, $class;
+}
+
+sub begin {
+    my ($self, $profile_name) = @_;
+    $self->{timers}->{$profile_name} = Time::Moment->now;
+}
+
+sub end {
+    my ($self, $profile_name) = @_;
+    my $before = $self->{timers}->{$profile_name} or die "profile $profile_name is not begun";
+    my $now = Time::Moment->now;
+    my $duration = $before->delta_milliseconds($now);
+    say { $self->{log_fh} } "name:$profile_name\ttime:$duration";
+    $self->{timers}->{$profile_name} = undef;
+}
+
+sub DESTROY {
+    my ($self) = @_;
+    close $self->{log_fh};
+}
 
 1;
 __END__
